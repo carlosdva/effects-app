0246d829074532fe1bd0005fe988b69b7fa98317
carlosdva <117352931+carlosdva@users.noreply.github.com>
2022-12-03 10:33:56 +0100

    Primer commit

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { filter, map, pluck } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private url = 'https://reqres.in/api'
+
+  constructor( private http: HttpClient ) { }
+
+  public getUsers() {
+    return this.http.get(`${this.url}/users?per_page=6`)
+      .pipe(
+        map(({data}:any) => data )
+      )
+  }
+}
